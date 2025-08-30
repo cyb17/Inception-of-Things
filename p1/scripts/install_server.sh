3ae7ea9dbcf465fa7df260063fbcafe8778fd9a1
cyb17 <yabingc7@gmail.com>
2025-08-30 16:26:19 +0200

    feat: add scripts for k3s server and agent installation

---
+++ p1/scripts/install_server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Stop script if any command fails
+set -e
+
+echo -e "${YELLOW}k3s server installing...${NC}"
+curl -sfL https://get.k3s.io | sh -
+
+if which k3s > /dev/null 2>&1; then
+    echo -e "${GREEN}k3s installed with success !${NC}"
+else
+    echo -e "${RED}failed to install k3s${NC}"
+    exit 1
+fi
+
+# Create shared folder and copy necessary data
+echo -e "${YELLOW}Creating shared folder and data for k3s Agent installation...${NC}"
+mkdir -p /vagrant/shared
+
+cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/
+hostname -i > /vagrant/shared/server-ip
+
+echo -e "${RED}Take a look...${NC}"
+cat /vagrant/shared/server-ip
+cat /vagrant/shared/node-token
+
---
+++ p1/scripts/install_worker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+set -e
+
+echo -e "${YELLOW}k3s agent installing...${NC}"
+
+TOKEN=$(cat /vagrant/shared/node-token)
+SERVER_IP=$(cat /vagrant/shared/server-ip)
+
+if curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$TOKEN" sh -; then
+    echo -e "${GREEN}k3s agent installed with success!${NC}"
+else
+    echo -e "${RED}Failed to install k3s agent${NC}"
+    exit 1
+fi
+
