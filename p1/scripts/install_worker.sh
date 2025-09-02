ad8b3b96ab037dd148d6ffd901e482b6c7247fcc
cyb17 <yabingc7@gmail.com>
2025-09-02 17:46:55 +0200

    have to resolve vm connection pb...

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-RED='\033[1;31m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
 # Stop script if a command fails
 set -e
 
-echo -e "${YELLOW}k3s agent installing...${NC}"
+echo "K3S AGENT INSTALLING..."
 
 TOKEN=$(cat /vagrant/shared/node-token)
 SERVER_IP=$(cat /vagrant/shared/server-ip)
 
-if curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$TOKEN" sh -; then
-    echo -e "${GREEN}k3s agent installed with success!${NC}"
-else
-    echo -e "${RED}Failed to install k3s agent${NC}"
-    exit 1
-fi
+curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$TOKEN" sh -
 
+echo '=> SHOW STATUS : '
+echo "$(sudo systemctl status k3s-agent)"
