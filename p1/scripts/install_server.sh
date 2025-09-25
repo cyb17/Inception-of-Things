256f029916e58789ac168ac26ab8179902adfc36
yachen <yachen@Iof-debian13.Iof.virtualbox.org>
2025-09-25 14:46:44 +0200

    p1 ok

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -4,7 +4,7 @@ set -e
 echo "Installing K3s server..."
 
 # Forcing K3s to bind on the private network
-curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110 --bind-address=192.168.56.110
 
 # Make sure node-token is accessible by the agent
 mkdir -p /vagrant/shared
