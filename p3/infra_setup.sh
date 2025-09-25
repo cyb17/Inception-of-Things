f141140fc6430b6e572ffc862d07973ecd9a27b4
yachen <yachen@Iof-debian13.Iof.virtualbox.org>
2025-09-25 13:20:37 +0200

    p3 ok

--- p3/infra_setup.sh
+++ p3/infra_setup.sh
@@ -9,7 +9,7 @@ ARGOCD_NAMESPACE="argocd"
 DEV_NAMESPACE="dev"
 
 GIT_REPO_URL="https://github.com/cyb17/yachen.git"
-GIT_REPO_PATH="yachen"
+GIT_REPO_PATH="dev"
 GIT_REPO_BRANCH="main"
 APP_NAME="my-app"
 
@@ -17,7 +17,8 @@ APP_NAME="my-app"
 #  Create K3d cluster
 # -----------------------------
 echo "🚀 Creating K3d cluster..."
-k3d cluster create $CLUSTER_NAME
+k3d cluster create $CLUSTER_NAME --port "30081:30081"
+echo '---------------------------------'
 
 # -----------------------------
 #  Create namespaces
@@ -25,26 +26,23 @@ k3d cluster create $CLUSTER_NAME
 echo "🚀 Creating namespaces..."
 kubectl create namespace $ARGOCD_NAMESPACE || echo "Namespace $ARGOCD_NAMESPACE exists"
 kubectl create namespace $DEV_NAMESPACE || echo "Namespace $DEV_NAMESPACE exists"
+echo '---------------------------------'
 
 # -----------------------------
 #  Install ArgoCD
 # -----------------------------
 echo "🚀 Installing ArgoCD..."
 kubectl apply -n $ARGOCD_NAMESPACE -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-# -----------------------------------------------------
-#  expose ArgoCD with a forward-port 8080 in background
-# -----------------------------------------------------
-#echo "🚀 expose ArgoCD to be accessible from host..."
-#kubectl port-forward svc/argocd-server -n argocd 8080:80 --address=0.0.0.0 &
+echo '---------------------------------'
 
 # --------------------------------------
 #  Display ArgoCD initial admin password
 # --------------------------------------
 kubectl -n argocd wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server --timeout=120s
-kubectl -n argocd get secret argocd-initial-admin-secret \
-  -o jsonpath="{.data.password}" | base64 -d
-echo
+echo "🚀 To access ArgoCD..."
+echo 'username : admin'
+echo "password : $(argocd admin initial-password -n argocd)"
+echo '---------------------------------'
 
 # -----------------------------
 #  Create ArgoCD Application
@@ -72,6 +70,7 @@ spec:
       selfHeal: true
 EOF
 
-echo "🎉 Setup complete! ArgoCD can be accessed from host wor vm at http://localhost:8080"
-echo "The app '$APP_NAME' will be automatically deployed to namespace '$DEV_NAMESPACE'."
+echo '---------------------------------'
+echo "🎉 Setup complete! Argocd can be accessed now from host at https://localhost:8080 OR vm at https://localhost:8080"
+echo '---------------------------------'
 
